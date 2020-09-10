5bb8ce760c3d944189c9cad08fd30914abf9366a
ReinTumbal <reintumbal@gmail.com>
2020-09-10 14:57:14 +0700

    7 Commit

---
+++ pertemuan_7/index.js
@@ -0,0 +1,97 @@
+// Conditon
+
+// // 1. If, else if, else
+// let Nilai = 95;
+
+// if(Nilai>80){
+//     console.log("Sangat Memuaskan");
+// }else if (nilai >= 60 && nilai <=80){
+//     console.log("Memuaskan");
+// }else {
+//     console.log("Jangan Menyerah");
+// }
+
+// 2. Switch Case
+// let seragamSekolah;
+// let hari = "senin";
+
+// switch (hari){
+//     case "senin":
+//         seragamSekolah = "kemeja putih bawahan merah";
+//         break;
+//     case "selasa":
+//         seragamSekolah = "kemeja hijau bawahan hitam";
+//         break;
+//     case "rabu":
+//         seragamSekolah = "kemeja putih bawahan putih";
+//         break;
+//     case "kamis":
+//         seragamSekolah = "kemeja batik merah bawahan hitam";
+//         break;
+//     case "jumat":
+//         seragamSekolah = "kemeja pramuka";
+//         break;
+//     default:
+//         seragamSekolah = "baju bebas";
+            
+// }
+// console.log(seragamSekolah);
+
+
+// let seragamSekolah;
+// let hari = "senin";
+
+// switch (hari){
+//     case "senin":
+//     case "selasa":
+//     case "rabu":
+//     case "kamis":
+//         seragamSekolah = "kemeja merah bawahan putih";
+//         break;
+//     case "jumat":
+//         seragamSekolah = "kemeja pramuka";
+//         break;
+//     default:
+//         seragamSekolah = "baju bebas";
+            
+// }
+// console.log(seragamSekolah);
+
+
+// const buku = {
+//     judul: "Aplha And Omega Jilid 1",
+//     Penulis: "Ellen G. White",
+//     tahun: 1827
+// };
+
+
+// 3. for/in Loop
+// for (i in buku){
+//     console.log(i, ':', buku[i]);
+// }
+
+// const buku = ["game of Thrones: A Song of ice and fire",
+//             "Harry Potter and the philosopher's Stone",
+//             "Lord Of The Rings: The Fellowship of The Ring"
+// ];
+
+
+// 4. For/of Loop
+// for (i of buku){
+//     console.log(i);
+// }
+
+
+// While Loop
+// let i = 1; 
+// while (i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+// 5. Do While Loop
+let i = 1;
+do{
+    console.log(i);
+    i++;
+}while (i <= 10);
