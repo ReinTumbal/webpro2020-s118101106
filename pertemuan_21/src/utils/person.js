49e1ad350bda922f0eb4f6917f36dcc966c3046a
ReinTumbal <reintumbal@gmail.com>
2020-11-26 12:08:44 +0700

    Commit WebPro

---
+++ pertemuan_21/src/utils/person.js
@@ -0,0 +1,26 @@
+export const person = [
+    {
+        name: "Reinhard Jackson Tumbal",
+        institution: "Unklab",
+        address: "Manado",
+        phone: "089505725260",
+    },
+    {
+        name: "Jaxx",
+        institution: "unsrat",
+        address: "indo",
+        phone: "0811xxxxxxx",
+    },
+    {
+        name: "Bexx",
+        institution: "Unima",
+        address: "Manado",
+        phone: "0898xxxxxx",
+    },
+    {
+        name: "keyy",
+        institution: "Della",
+        address: "Tikala",
+        phone: "0821xxxxxx",
+    },
+    ];
